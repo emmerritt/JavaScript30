468ab14cd53b37bdb852c84b56ead8f9a3157c00
emily merritt <emmerritt@mac.com>
2019-05-05 13:35:12 -0700

    I'm up to Module #18!
    Uploading all of my completed code, and removing the pre-made completed projects

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,76 @@
+/* Get player elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const fullScreen = player.querySelector('[name="fullscreen"]');
+
+/* Build player functions */
+function togglePlay() {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function spaceBarPlay(e) {
+    const key = e.keyCode;
+    if (key === 32) {
+        togglePlay();
+    }
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+// trying to get fullscreen working. works to initiate, but not exit
+function toggleFullScreen() {
+    video.webkitEnterFullScreen();
+}
+
+/* Hook up event listeners */
+
+toggle.addEventListener('click', togglePlay);
+
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+window.addEventListener('keyup', spaceBarPlay);
+
+fullScreen.addEventListener('click', toggleFullScreen);
